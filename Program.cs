fc05e6b05fb2450e40027592e200ed4aec9d7549
naunmurillo <56647089+naunmurillo@users.noreply.github.com>
2020-01-03 10:00:14 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proxy.Clases;
+namespace Proxy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int codigotarjeta = 532543463;
+            int codigoSeguridad = 1038948470;
+
+            Central central = new Central(codigotarjeta, codigoSeguridad);
+
+            Tarjeta tarjetaSimple = new TarjetaReal(codigotarjeta);
+            tarjetaSimple.RealizarContacto(central);
+
+            Tarjeta proxy = new TarjetaProxy(tarjetaSimple);
+            proxy.RealizarContacto(central);
+
+
+            Console.ReadKey();
+        }
+
+
+       
+    }
+}
